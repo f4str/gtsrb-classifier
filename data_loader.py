9fad7b0e78e22315682a985845fa727414b81ffa
Farhan Ahmed <farhan.ahmed.1@stonybrook.edu>
2020-02-24 21:34:47 -0500

    finished classifier

--- data_loader.py
+++ data_loader.py
@@ -5,19 +5,27 @@ def load_training_data(file='./data/train.p'):
 	with open(file, mode='rb') as f:
 		data = pickle.load(f)
 	X_train, y_train = data['features'], data['labels']
-	X_train = X_train.astype(np.float32) / 255
+	X_train = (255 - X_train.astype(np.float32)) / 255
 	return X_train, y_train
 
 def load_validation_data(file='./data/valid.p'):	
 	with open(file, mode='rb') as f:
 		data = pickle.load(f)
 	X_valid, y_valid = data['features'], data['labels']
-	X_valid = X_valid.astype(np.float32) / 255
+	X_valid = (255 - X_valid.astype(np.float32)) / 255
 	return X_valid, y_valid
 	
 def load_testing_data(file='./data/test.p'):
 	with open(file, mode='rb') as f:
 		data = pickle.load(f)
 	X_test, y_test = data['features'], data['labels']
-	X_test = X_test.astype(np.float32) / 255
+	X_test = (255 - X_test.astype(np.float32)) / 255
 	return X_test, y_test
+
+if __name__ == "__main__":
+	x, y = load_testing_data()
+	print(f'x.shape = {x.shape}, x.dtype = {x.dtype}')
+	print(f'x[0].shape = {x[0].shape}, x[0].dtype = {x[0].dtype}')
+	print(f'x[0,0].shape = {x[0,0].shape}, x[0,0].dtype = {x[0,0].dtype}')
+	print(f'x[0,0,0].shape = {x[0,0,0].shape}, x[0,0,0].dtype = {x[0,0,0].dtype}')
+	print(f'x[0,0,0] = {x[0,0,0]}')
--- network.py
+++ network.py
@@ -10,25 +10,26 @@ adam optimizer
 
 import tensorflow as tf
 import numpy as np
+from sklearn.utils import shuffle
 import data_loader
 
 def convolution_layer(input, channels, filters, filter_size=5, strides=1):
-	weights = tf.Variable(tf.random_normal([filter_size, filter_size, channels, filters]))
-	biases = tf.Variable(tf.random_normal([filters]))
-	layer = tf.nn.conv2d(input, filter=weights, strides=[1, strides, strides, 1], padding='SAME') + biases
+	weights = tf.Variable(tf.truncated_normal(shape=[filter_size, filter_size, channels, filters], mean=0, stddev=0.1))
+	biases = tf.Variable(tf.zeros([filters]))
+	layer = tf.nn.conv2d(input, filter=weights, strides=[1, strides, strides, 1], padding='VALID') + biases
 	return tf.nn.relu(layer)
 
-def pooling_layer(input, strides=1, k=2):
-	return tf.nn.max_pool(input, ksize=[1, k, k, 1], strides=[1, k, k, 1], padding='SAME')
+def pooling_layer(input, k=2):
+	return tf.nn.max_pool(input, ksize=[1, k, k, 1], strides=[1, k, k, 1], padding='VALID')
 
 def flatten_layer(layer):
 	layer_shape = layer.get_shape()
 	num_features = layer_shape[1:4].num_elements()
-	return tf.reshape(layer, [-1, num_features]), num_features
+	return tf.reshape(layer, [-1, num_features])
 
 def fully_connected_layer(input, num_inputs, num_outputs, relu=True):
-	weights = tf.Variable(tf.random_normal([num_inputs, num_outputs]))
-	biases = tf.Variable(tf.random_normal([num_outputs]))
+	weights = tf.Variable(tf.truncated_normal(shape=[num_inputs, num_outputs], mean=0, stddev=0.1))
+	biases = tf.Variable(tf.zeros([num_outputs]))
 	layer = tf.matmul(input, weights) + biases
 	if relu:
 		return tf.nn.relu(layer)
@@ -47,23 +48,16 @@ class NeuralNetwork:
 		self.build()
 	
 	def load_data(self):
-		X_train, y_train = data_loader.load_training_data()
+		self.X_train, self.y_train = data_loader.load_training_data()
+		self.X_valid, self.y_valid = data_loader.load_validation_data()
 		self.X_test, self.y_test = data_loader.load_testing_data()
 		
-		self.img_shape = list(X_train[0].shape)
-		self.num_classes = len(np.unique(y_train))
-		
-		dataset = tf.data.Dataset.from_tensor_slices((X_train, y_train))
-		dataset = dataset.shuffle(100)
-		dataset = dataset.repeat(3)
-		dataset = dataset.batch(self.batch_size)
-		iterator = dataset.make_one_shot_iterator()
-		self.x_batch, self.y_batch = iterator.get_next()
+		self.img_shape = list(self.X_train[0].shape)
+		self.num_classes = len(np.unique(self.y_train))
 	
 	def build(self):
 		self.x = tf.placeholder(tf.float32, [None] + self.img_shape)
 		self.y = tf.placeholder(tf.int32, [None])
-		self.one_hot_y = tf.one_hot(self.y, self.num_classes)
 		
 		# Convolution 1: 32x32x3 -> 28x28x6 + ReLU
 		conv1_channels = 3
@@ -78,10 +72,11 @@ class NeuralNetwork:
 		# Pooling: 10x10x16 -> 5x5x16
 		pool2 = pooling_layer(conv2)
 		# Flatten: 5x5x16 -> 400
-		flat, num_features = flatten_layer(pool2)
+		flat = flatten_layer(pool2)
 		# Fully Connected 1: 400 -> 120
+		fc1_input = 400
 		fc1_output = 120
-		fc1 = fully_connected_layer(flat, num_features, fc1_output)
+		fc1 = fully_connected_layer(flat, fc1_input, fc1_output)
 		# Fully Connected 2: 120 -> 84
 		fc2_input = 120
 		fc2_output = 84
@@ -89,27 +84,38 @@ class NeuralNetwork:
 		# Logits: 84 -> 43
 		logits_input = 84
 		logits_output = 43
-		self.logits = fully_connected_layer(fc2, logits_input, logits_output, relu=False)
+		logits = fully_connected_layer(fc2, logits_input, logits_output, relu=False)
 		
-		self.prediction = tf.nn.softmax(self.logits)
+		one_hot_y = tf.one_hot(self.y, self.num_classes)
 		
-		cross_entropy = tf.nn.softmax_cross_entropy_with_logits_v2(logits=self.logits, labels=self.one_hot_y)
+		cross_entropy = tf.nn.softmax_cross_entropy_with_logits_v2(logits=logits, labels=one_hot_y)
 		self.loss = tf.reduce_mean(cross_entropy)
 		
 		self.optimizer = tf.train.AdamOptimizer(learning_rate=self.learning_rate).minimize(self.loss)
 		
-		correct_prediction = tf.equal(tf.argmax(self.prediction, axis=1), tf.argmax(self.one_hot_y, axis=1))
+		correct_prediction = tf.equal(tf.argmax(logits, axis=1), tf.argmax(one_hot_y, axis=1))
 		self.accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+		self.prediction = tf.argmax(logits, axis=1)
 	
 	def train(self, epochs = 100):
 		self.sess.run(tf.global_variables_initializer())
 		
+		print('training start')
+		
 		for i in range(epochs):
-			feed_dict = {self.x: self.x_batch.eval(session=self.sess), self.y: self.y_batch.eval(session=self.sess)}
-			self.sess.run(self.optimizer, feed_dict=feed_dict)
+			X_data, y_data = shuffle(self.X_train, self.y_train)
+			total_acc = 0
+			for offset in range(0, len(y_data), self.batch_size):
+				end = offset + self.batch_size
+				batch_x, batch_y = X_data[offset:end], y_data[offset:end]
+				feed_dict = {self.x: batch_x, self.y: batch_y}
+				self.sess.run(self.optimizer, feed_dict=feed_dict)
+				loss, acc = self.sess.run([self.loss, self.accuracy], feed_dict=feed_dict)
+				total_acc += acc * len(batch_y)
 			
+			feed_dict = {self.x: self.X_valid, self.y: self.y_valid}
 			loss, acc = self.sess.run([self.loss, self.accuracy], feed_dict=feed_dict)
-			print(f'epoch {i + 1}: loss = {loss:.4f}, training accuracy = {acc:.4f}')
+			print(f'epoch {i + 1}: loss = {loss:.4f}, training accuracy = {total_acc / len(y_data):.4f}, validation accuracy = {acc:.4f}')
 			
 		print('training complete')
 		
@@ -123,4 +129,4 @@ class NeuralNetwork:
 
 if __name__ == '__main__':
 	net = NeuralNetwork()
-	net.train(200)
+	net.train(20)
